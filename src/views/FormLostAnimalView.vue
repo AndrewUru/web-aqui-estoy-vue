80fdc813432f4e11791f530ec9e1d8761b3bfe4c
Vio <vioeasd@gmail.com>
2025-01-27 15:57:09 +0000

    arreglo iconos de la lost card

--- src/views/FormLostAnimalView.vue
+++ src/views/FormLostAnimalView.vue
@@ -9,7 +9,7 @@
         </label>
         <label class="label-group a">Última vez visto</label>
         <label for="date"> <em>Fecha:</em>
-          <input type="date" name="date" id="date" v-model="dateForm" required>
+          <input type="date" name="date" id="date" v-model="formData.date" required>
         </label>
         <label for="time"><em>Hora:</em>
           <input type="text" placeholder="Ejemplo 00:00" name="time" id="time" v-model="formData.time" required>
@@ -80,35 +80,25 @@ export default {
         content: '',
       },
       formData: {
-        name: '',
-        date: '',
-        time: '',
-        country: '',
-        city: '',
-        street: '',
-        number: '',
+        name: 'Toby',
+        date: '2025-01-01',
+        time: '01:01',
+        country: 'España',
+        city: 'Las Palmas de Gran Canaria',
+        street: 'Calle falsa',
+        number: '1',
         reward: '',
-        email: '',
-        description: '',
+        email: 'encontrar_a_toby@helpme.com',
+        description: 'perdido bla bla bla',
         URLImage: null,
         uid: ''
       },
-      dateForm: '',
     }
   },
   components: {
     ModalComponent
   },
   methods: {
-    dateFormat() {
-      if (this.dateForm) {
-        const datef = new Date(this.dateForm);
-        console.log(datef);
-        this.formData.date = datef.toLocaleDateString();
-      }
-
-    },
-
     async onSubmit() {
       console.log(this.formData)
       console.log("valor de isChecked: " + this.isChecked);
